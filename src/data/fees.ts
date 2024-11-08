a961c840d34044bc4c05356acb0c6ec0728f09bf
tomcorey26 <tomcorey26@my.uri.edu>
2024-11-08 15:51:51 -0500

    New feature fees

---
+++ src/data/fees.ts
@@ -0,0 +1,23 @@
+// Dues Reminder:
+// Venmo:
+interface PlayerFee {
+  name: string;
+  paid: boolean;
+}
+
+export const fees: PlayerFee[] = [
+  { name: 'Christian', paid: true },
+  { name: 'CJ', paid: false },
+  { name: 'Dylan', paid: false },
+  { name: 'Jason', paid: false },
+  { name: 'Jeff', paid: false },
+  { name: 'John', paid: false },
+  { name: 'KC', paid: false },
+  { name: 'Kobi', paid: false },
+  { name: 'Lucas', paid: false },
+  { name: 'Mike', paid: false },
+  { name: 'Tom', paid: true },
+  { name: 'Zac', paid: false },
+];
+
+const total_cost = 1145;
