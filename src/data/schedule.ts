46bc1ae98aaa0edff7cadf4dca2775bc878d084b
tomcorey26 <tomcorey26@my.uri.edu>
2024-11-08 13:30:53 -0500

    Add schedule

---
+++ src/data/schedule.ts
@@ -0,0 +1,74 @@
+export const schedule = [
+  {
+    summary: 'Lightning  FC L 6-7 vs Pique Blinders FC',
+    location:
+      'North Kingstown on Field 3 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2024-11-05T22:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Kings FC',
+    location:
+      'North Kingstown on Field 2 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2024-11-12T21:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Temptation Haaland',
+    location:
+      'North Kingstown on Field 3 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2024-11-19T21:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Ted’s Team',
+    location:
+      'North Kingstown on Field 2 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2024-11-26T21:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Ball Busters',
+    location:
+      'North Kingstown on Field 3 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2024-12-10T22:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Wong',
+    location:
+      'North Kingstown on Field 2 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2024-12-17T21:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Pique Blinders FC',
+    location:
+      'North Kingstown on Field 2 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2025-01-07T21:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+  {
+    summary: 'Lightning  FC vs Kings FC',
+    location:
+      'North Kingstown on Field 2 1610 Davisville Rd  North Kingstown RI 02852',
+    time: {
+      dateTime: '2025-01-14T21:00:00-05:00',
+      timeZone: 'America/New_York',
+    },
+  },
+];
